6b5ccc15d75c2f81d970c594d36a7211f44f151a
Anwar <anwarhermuche2@gmail.com>
2024-05-03 17:49:03 -0300

    Adicionei comentários explicativos, função para salvar o arquivo, atributos de número de arestas e vértices e um método para calcular o grau de um vértice

--- graph.py
+++ graph.py
@@ -9,13 +9,15 @@ class Grafo:
     self.vertices = vertices
     self.arestas = arestas
     self.direcionado = direcionado
+    self.nVertices = len(vertices) if vertices else 0
+    self.nArestas = len(arestas) if arestas else 0
 
   def __str__(self) -> str:
     return f"Vértices: {self.vertices}\nArestas: {self.arestas}"
 
   def _verificaFormato(self, grafo: str) -> bool:
     # Extraindo o padrão do formato requerido
-    padrao = r"V = \{\d+(?:,\d+)*\}; A = \{\(\d+,\d+\)(?:,\(\d+,\d+\))*\};"
+    padrao = r"V = \{\d+(?:, \d+)*\}; A = \{\(\d+, \d+\)(?:, \(\d+, \d+\))*\};"
     return bool(re.match(padrao, grafo))
   
   def _extrairValoresDoGrafo(self, grafo: str) -> Union[None, tuple[set, set]]:
@@ -28,7 +30,7 @@ class Grafo:
     vertices = set(map(int, numerosVertices.split(",")))
 
     # Extrai as tuplas para A
-    tuplasArestas = re.findall(r"\((\d+),(\d+)\)", grafo)
+    tuplasArestas = re.findall(r"\((\d+), (\d+)\)", grafo)
     arestas = set((int(x), int(y)) for x, y in tuplasArestas)
 
     return vertices, arestas
@@ -44,17 +46,26 @@ class Grafo:
     verticesEstranhos = verticesArestas.difference(vertices)
 
     return verticesArestas.issubset(vertices), verticesEstranhos
+  
+  def _salvaArquivo(self, vertices: set, arestas: set[tuple[int, int]]) -> None:
+    grafo = f"V = {vertices}; A = {arestas};"
+    with open(f'{self.nomeArquivo_}.txt', 'w') as file:
+      file.write(grafo)
+      file.close()
 
   def lerArquivo(self, nomeArquivo: str) -> None:
-    nomeArquivo = nomeArquivo[:-4] if nomeArquivo[-4:] == '.txt' else nomeArquivo
-    with open(f'{nomeArquivo}.txt', 'r') as file:
+    # Deixando o nome do arquivo sem o .txt
+    self.nomeArquivo_ = nomeArquivo[:-4] if nomeArquivo[-4:] == '.txt' else nomeArquivo
+    with open(f'{self.nomeArquivo_}.txt', 'r') as file:
       grafo = file.read()
+      file.close()
 
+    # Verificando se o formato do grafo é válido
     if self._verificaFormato(grafo):
       self.vertices, self.arestas = self._extrairValoresDoGrafo(grafo)
       grafoValido, verticesEstanhos = self._verificaArestasValidas(self.vertices, self.arestas)
 
-      # Verificamos se o grafo é válido
+      # Se as arestas não condizem com os vértices, então o grafo não é válido
       if not grafoValido: 
         raise ValueError(f"O grafo não é válido. Há arestas com vértices estranhos: {verticesEstanhos}")
       
@@ -65,36 +76,64 @@ class Grafo:
           aux.add((v2, v1))
         self.arestas = self.arestas.union(aux)
 
+      # Atualiza o número de vértices e arestas
+      self.nVertices = len(self.vertices)
+      self.nArestas = len(self.arestas)
+
     else:
       raise SyntaxError(f"Formato do arquivo incorreto.")
     
   def adicionaVertice(self, vertice: int) -> None:
+    # Se o vértice existe, não prosseguimos com a operação
     if vertice in self.vertices:
       print(f"Vértice {vertice} já foi adicionado")
     else:
       if type(vertice) == int:
         self.vertices.add(vertice)
+        print(f"Vértice {vertice} adicionado com sucesso")
+        self._salvaArquivo(self.vertices, self.arestas)
+
+        self.nVertices += 1
+
 
   def removeVertice(self, vertice: int) -> None:
     if vertice not in self.vertices:
       raise ValueError(f"Você não pode excluir um vértice que ainda não foi adicionado.")
+    # Removendo o vértice
     self.vertices.remove(vertice)
+    print(f"Vértice {vertice} removido com sucesso.")
+    self.nVertices -= 1
 
+    # Excluindo todas as arestas que estavam ligadas ao vértice excluído
     excluirArestas = set()
     for v1, v2 in self.arestas:
       if vertice == v1 or vertice == v2:
         excluirArestas.add((v1, v2))
+        self.nArestas -= 1
     self.arestas = self.arestas.difference(excluirArestas)
+
+    self._salvaArquivo(self.vertices, self.arestas)
   
   def adicionaAresta(self, aresta: tuple[int, int]) -> None:
     if aresta in self.arestas:
       print(f"Aresta {aresta} já foi adicionada.")
     else:
+      # Verificando o tipo da aresta
       if type(aresta) == tuple:
         if len(aresta) == 2 and type(aresta[0]) == int and type(aresta[1]) == int:
           self.arestas.add(aresta)
+          self.nArestas += 1
+
+          # Se for não direcionado, adicionamos o outro par de aresta complementar (para ter ida e volta)
           if not self.direcionado:
             self.arestas.add((aresta[1], aresta[0]))
+            self.nArestas += 1
+            print(f"Arestas {aresta} e {(aresta[1], aresta[0])} adicionadas com sucesso.")
+          else:
+            print(f"Aresta {aresta} adicionada com sucesso")
+
+          self._salvaArquivo(self.vertices, self.arestas)
+
         else:
           raise ValueError("Apenas dois inteiros são válidos por tupla.")
       else:
@@ -103,17 +142,42 @@ class Grafo:
   def removeAresta(self, aresta: tuple[int, int]) -> None:
     if aresta not in self.arestas:
       raise ValueError(f"Você não pode excluir uma aresta que ainda não foi adicionada.")
+    
+    # Removendo a aresta
     self.vertices.remove(aresta)
+    self.nArestas -= 1
+    
+    # Se o grafo for não direcionado, excluímos a aresta complementar (para não ter nem ida nem volta)
     if not self.direcionado:
       self.vertices.remove((aresta[1], aresta[0]))
+      self.nArestas -= 1
+      print(f"Arestas {aresta} e {(aresta[1], aresta[0])} removidas com sucesso.")
+    else:
+      print(f"Aresta {aresta} removida com sucesso.")
+
+    self._salvaArquivo(self.vertices, self.arestas)
+
+  def verGrau(self, vertice: int) -> int:
+    if vertice not in self.vertices:
+      raise ValueError(f"Vértice {vertice} não pertence ao grafo.")
+    
+    # Percorrendo as arestas e contando as vezes que o vértice está presente
+    grau = 0
+    for aresta in self.arestas:
+      grau += 1 if vertice in aresta else 0
+    
+    # Se for direcionado, retornamos o grau. Se for não direcionado, dividimos por 2, pois contamos duas vezes
+    return grau if self.direcionado else int(grau/2)
 
   def listaAdjacencia(self) -> dict:
+    # Inicializamos um dicionário para cada vértice e um conjunto vazio associado a ele
     lista = {vertice: set() for vertice in self.vertices}
     for v1, v2 in self.arestas:
       lista[v1].add(v2)
     return lista
   
   def matrizAdjacencia(self) -> list[list]:
+    # Inicializando uma matriz nxn, sendo n o número de vértices
     matriz = [[0]*len(self.vertices)]*len(self.vertices)
     for v1, v2 in self.arestas:
       matriz[v1-1][v2-1] = 1
